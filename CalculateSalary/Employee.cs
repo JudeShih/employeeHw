a47e3e34f744e2526a1371df115bc129101e7778
JudeShih <jackshih5828@gmail.com>
2015-05-19 11:57:04 +0800

    employeeHW

---
+++ CalculateSalary/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CalculateSalary
+{
+    class Employee
+    {
+        private int _baseSalary;
+        private int _benefit;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+      
+
+public int BaseSalary
+{
+  set
+  {
+      if (value < 0)
+      {
+          this._baseSalary = 0;
+          MessageBox.Show("baseSalary cannot be less than 0");
+      }
+      else
+          this._baseSalary = value;    
+  }
+     get { return _baseSalary; }
+}
+public int Benefit
+{
+    get { return _benefit; }
+    set { _benefit = value; }
+}
+public int Salary
+{
+    get { return BaseSalary + Benefit; }
+}
+    }
+}
---
+++ CalculateSalary/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateSalary
+{
+    public partial class Form1 : Form
+    {
+        Employee 施昊廷 = new Employee();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             int BaseSalary  = int.Parse(baseSalary.Text);
+             int Benefit= int.Parse(benefit.Text);
+             施昊廷.BaseSalary = BaseSalary;
+             施昊廷.Benefit = Benefit;
+            MessageBox.Show((施昊廷.Salary).ToString());
+        }
+    }
+}
